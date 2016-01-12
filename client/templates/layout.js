cffecc3d768959413be0a59f4ddd626bed437af1
roosness <roos95@live.nl>
2016-01-12 14:59:14 +0100

    Navigatie zit er nu bij. En ja, groot issie lelijk

--- client/templates/layout.js
+++ client/templates/layout.js
@@ -1,3 +1,10 @@
 Template.layout.events({
-
+	'click .nav-knop': function() {
+		console.log("go");
+    $('nav').toggleClass("inactive");
+    $('.nav-knop').toggleClass("omhoog");
+    $('.overlay').toggleClass("donker");
+    
+}
 });
+
