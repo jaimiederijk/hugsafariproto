16ec40b924576fea6503b0a45d7768f1141450c7
jaimiederijk <j.r.derijk@quicknet.nl>
2016-01-14 16:30:40 +0100

    leaderboard werkend

--- client/templates/leaderboard.js
+++ client/templates/leaderboard.js
@@ -1,8 +1,15 @@
 Template.leaderboard.helpers({
+  teams: function(){
+    return Teams.find()
+  },
+  findCount: function() {
+    return this.foundtargets.length
+  },
   points:function(){
     //debugger;
-    var teamId = Meteor.user().profile.teamid
-    var teamChallenges=Teams.findOne(teamId).challenges;
+
+    //var teamId = Meteor.user().profile.teamid
+    var teamChallenges=this.challenges;
     var totalPoints=0;
     for (var i = 0; i < teamChallenges.length; i++) { 
       totalPoints += teamChallenges[i].punten
@@ -12,5 +19,17 @@ Template.leaderboard.helpers({
 });
 
 Template.leaderboard.events({
-
+  'click #challengesleaderbord' :function (event ,template) {
+    $('.challengelist').toggleClass("displayNone");
+    $('.gevondenlist').toggleClass("displayNone");
+    $('#challengesleaderbord').attr('disabled',true).addClass("disabled");
+    $('#gevondenleaderbord').attr('disabled',false).removeClass("disabled");
+  },
+  'click #gevondenleaderbord' :function (event ,template) {
+    $('.gevondenlist').toggleClass("displayNone");
+    $('.challengelist').toggleClass("displayNone");
+    
+    $('#challengesleaderbord').attr('disabled',false).removeClass("disabled");
+    $('#gevondenleaderbord').attr('disabled',true).addClass("disabled");
+  }
 });
